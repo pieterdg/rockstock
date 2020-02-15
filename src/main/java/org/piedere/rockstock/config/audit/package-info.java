c69a7fa13790c67bf4abe184da2dcd3ca83eaacf
Pieter De Graef <piedere@gmail.com>
2020-02-15 12:48:51 +0100

    Initial version of rockstock generated by JHipster-6.7.1

---
+++ src/main/java/org/piedere/rockstock/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.piedere.rockstock.config.audit;
---
+++ src/main/java/org/piedere/rockstock/web/rest/errors/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Specific errors used with Zalando's "problem-spring-web" library.
+ *
+ * More information on https://github.com/zalando/problem-spring-web
+ */
+package org.piedere.rockstock.web.rest.errors;
