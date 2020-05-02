6f612efa3939bced805a08c20ec20e095d9fe5f9
Pieter De Graef <piedere@gmail.com>
2020-05-02 13:01:13 +0200

    Renewed location page, with pictures.

--- src/main/webapp/app/entities/location/location-detail.component.ts
+++ src/main/webapp/app/entities/location/location-detail.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpResponse } from '@angular/common/http';
 
 import { ILocation } from 'app/shared/model/location.model';
+import { IPicture } from 'app/shared/model/picture.model';
+import { PictureSearchService } from 'app/shared/search/picture-search.service';
 
-@Component({
-  selector: 'jhi-location-detail',
-  templateUrl: './location-detail.component.html'
-})
+@Component( {
+    selector: 'jhi-location-detail',
+    templateUrl: './location-detail.component.html'
+} )
 export class LocationDetailComponent implements OnInit {
-  location: ILocation | null = null;
+    location: ILocation | null = null;
+    pictures: IPicture[] = [];
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+    constructor( protected activatedRoute: ActivatedRoute, private pictureSearchService: PictureSearchService ) { }
 
-  ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ location }) => (this.location = location));
-  }
+    ngOnInit(): void {
+        this.activatedRoute.data.subscribe(( { location } ) => this.onLocationReceived( location ) );
+    }
 
-  previousState(): void {
-    window.history.back();
-  }
+    private onLocationReceived( location: ILocation ): void {
+        this.location = location;
+        if ( location ?.id ) {
+            this.pictureSearchService.getThumbnailsForLocation( location ?.id, 250, 0 ).subscribe(( res: HttpResponse<IPicture[]> ) => this.onPicturesReceived( res.body || [] ) );
+        }
+    }
+
+    private onPicturesReceived( pictures: IPicture[] ): void {
+        this.pictures = pictures;
+    }
+
+    previousState(): void {
+        window.history.back();
+    }
 }
